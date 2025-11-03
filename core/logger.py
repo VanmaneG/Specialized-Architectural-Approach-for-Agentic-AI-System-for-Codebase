bfe9453d09155f4ebf91c7d4dd5560c452a86c25
VanmaneG <gauravvanmane98@gmail.com>
2025-11-03 15:13:32 +0530

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -2,6 +2,9 @@ import logging
 from rich.logging import RichHandler
 
 def setup_logger(name=__name__):
+    """
+    for logging purpose
+    """
     logging.basicConfig(
         level="INFO",
         format="%(message)s",
